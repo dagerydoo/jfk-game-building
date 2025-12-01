9fd4e430c547388e3034ce5872325267166a01fd
Andrew Hannon <me@local.host>
2025-11-30 21:57:06 -0500

    Added RightPaddle controls

--- wall-ball/game_board.gd
+++ wall-ball/game_board.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var left_paddle = $LeftPaddle
+@onready var right_paddle = $RightPaddle
 
 @export var paddle_speed = 600.0
 
@@ -9,3 +10,7 @@ func _physics_process(delta: float) -> void:
 		left_paddle.position += paddle_speed * Vector2.UP * delta
 	if Input.is_action_pressed("left_paddle_down"):
 		left_paddle.position += paddle_speed * Vector2.DOWN * delta
+	if Input.is_action_pressed("right_paddle_up"):
+		right_paddle.position += paddle_speed * Vector2.UP * delta
+	if Input.is_action_pressed("right_paddle_down"):
+		right_paddle.position += paddle_speed * Vector2.DOWN * delta
