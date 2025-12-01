e9ae2505ddbd2c34361b90a5b5de882286ba4308
Andrew Hannon <me@local.host>
2025-11-30 22:16:22 -0500

    Emitting signal when left/right walls hit

--- wall-ball/walls.gd
+++ wall-ball/walls.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+signal ball_hit_left_wall()
+signal ball_hit_right_wall()
+
 func _on_right_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_meta("is_ball") and body.get_meta("is_ball"):
-		print("right wall hit")
+		ball_hit_right_wall.emit()
 
 func _on_left_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_meta("is_ball") and body.get_meta("is_ball"):
-		print("left wall hit")
+		ball_hit_left_wall.emit()
